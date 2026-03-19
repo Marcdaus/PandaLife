a1c6ab4ec13760ccf0ab7814ac1c2277a4f47c13
daus_dog <mvmsueca@gmail.com>
2026-03-19 17:04:10 +0100

    plantar varias bayas y bambus
    Agregar al script de Show la opción de plantar mas tipos de plantas

--- Assets/[PandaLife]/Farm/Interactives/Farming_area/Sow.cs
+++ Assets/[PandaLife]/Farm/Interactives/Farming_area/Sow.cs
@@ -3,7 +3,15 @@ using UnityEngine;
 public class Sow : Interactuable
 {
     private FarmingArea area;
-    public GameObject cropprefab;
+    public GameObject cropbamboo;
+    public GameObject cropreddragon;
+    public GameObject cropblue_Berry;
+    public GameObject cropuchuva;
+    // Declaramos el enum
+    private enum Cultivo { bamboo, reddragon, blueberry, uchuva }
+
+    // Esta es la variable que aparecerá en el Inspector
+    [SerializeField] private Cultivo cultivoSeleccionado;
 
     void Awake()
     {
@@ -16,16 +24,34 @@ public class Sow : Interactuable
 
         if (!area.ThereIsSomething)
         {
-
-            GameObject crop = Instantiate(cropprefab, area.spawnpoint.position, Quaternion.identity);
-
-            //poner el Crop como hijo de la parcela
-            crop.transform.SetParent(area.transform);
-
-            area.ThereIsSomething = true; // marca terreno ocupado
-
-            // asignar el FarmingArea a la planta recién creada
-            crop.GetComponent<Harvest>().area = area;
+            if (cultivoSeleccionado == Cultivo.bamboo)
+            {
+                GameObject crop = Instantiate(cropbamboo, area.spawnpoint.position, Quaternion.identity);
+                crop.transform.SetParent(area.transform);
+                area.ThereIsSomething = true;
+                crop.GetComponent<Harvest>().area = area;
+            }
+            else if (cultivoSeleccionado == Cultivo.reddragon)
+            {
+                GameObject crop = Instantiate(cropreddragon, area.spawnpoint.position, Quaternion.identity);
+                crop.transform.SetParent(area.transform);
+                area.ThereIsSomething = true;
+                crop.GetComponent<Harvest>().area = area;
+            }
+            else if (cultivoSeleccionado == Cultivo.blueberry)
+            {
+                GameObject crop = Instantiate(cropblue_Berry, area.spawnpoint.position, Quaternion.identity);
+                crop.transform.SetParent(area.transform);
+                area.ThereIsSomething = true;
+                crop.GetComponent<Harvest>().area = area;
+            }
+            else if (cultivoSeleccionado == Cultivo.uchuva)
+            {
+                GameObject crop = Instantiate(cropuchuva, area.spawnpoint.position, Quaternion.identity);
+                crop.transform.SetParent(area.transform);
+                area.ThereIsSomething = true;
+                crop.GetComponent<Harvest>().area = area;
+            }
 
             area.sowing();
             area.ThereIsSomething = true; 
