e8df27d69c191c9ea87820b73565b11587b8905f
Jose <josegandia2003@gmail.com>
2026-04-02 22:21:11 +0200

    Pin fantasma fix

--- Assets/[PandaLife]/PinsUI/Scripts/PinUIElement.cs
+++ Assets/[PandaLife]/PinsUI/Scripts/PinUIElement.cs
@@ -26,9 +26,24 @@ public abstract class PinUIElement : MonoBehaviour
     {
         if (worldPosition)
         {
-            Vector2 pos = camera.WorldToViewportPoint(worldPosition.position);
-            rect.anchorMax = pos;
-            rect.anchorMin = pos;
+            // Usamos Vector3 para poder leer la Z
+            Vector3 pos = camera.WorldToViewportPoint(worldPosition.position);
+
+            // Si la Z es menor que 0, significa que el objeto está a nuestra espalda
+            if (pos.z < 0)
+            {
+                // Lo oculto
+                rect.localScale = Vector3.zero;
+            }
+            else
+            {
+                // Si esta delante su tamaño
+                rect.localScale = Vector3.one;
+
+                // Actualizamos su posición en la pantalla
+                rect.anchorMax = (Vector2)pos;
+                rect.anchorMin = (Vector2)pos;
+            }
         }
     }
     //=========================================================================================================================
