a2899df9b2dad90273f7932c23ad9033424c9644
Jose <josegandia2003@gmail.com>
2026-04-03 11:31:01 +0200

    Pin Animation Fix

--- Assets/[PandaLife]/PinsUI/Scripts/PinUIElement.cs
+++ Assets/[PandaLife]/PinsUI/Scripts/PinUIElement.cs
@@ -26,23 +26,17 @@ public abstract class PinUIElement : MonoBehaviour
     {
         if (worldPosition)
         {
-            // Usamos Vector3 para poder leer la Z
-            Vector3 pos = camera.WorldToViewportPoint(worldPosition.position);
+            Vector3 viewportPos = camera.WorldToViewportPoint(worldPosition.position);
 
-            // Si la Z es menor que 0, significa que el objeto está a nuestra espalda
-            if (pos.z < 0)
+            if (viewportPos.z < 0)
             {
-                // Lo oculto
-                rect.localScale = Vector3.zero;
+                rect.anchorMax = new Vector2(-2f, -2f);
+                rect.anchorMin = new Vector2(-2f, -2f);
             }
             else
             {
-                // Si esta delante su tamaño
-                rect.localScale = Vector3.one;
-
-                // Actualizamos su posición en la pantalla
-                rect.anchorMax = (Vector2)pos;
-                rect.anchorMin = (Vector2)pos;
+                rect.anchorMax = (Vector2)viewportPos;
+                rect.anchorMin = (Vector2)viewportPos;
             }
         }
     }
