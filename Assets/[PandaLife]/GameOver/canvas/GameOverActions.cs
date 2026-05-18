0be2fa3ee534e4b8bb8cb6a25ff5389ceedc7019
“Marcdaus” <mvmsueca@gmail.com>
2026-05-18 19:23:46 +0200

    Rematar gameover

---
+++ Assets/[PandaLife]/GameOver/canvas/GameOverActions.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverActions : MonoBehaviour
+{
+    [SerializeField] private GameString HomeScene;
+    [SerializeField] private GameString MainmenuScene;
+
+
+    public void MainMenu()
+    {
+        GameManager.instance.Resetplay();
+        SceneManager.LoadScene(MainmenuScene.Value);
+
+    }
+    public void Replay()
+    {
+
+        GameManager.instance.Resetplay();
+        SceneManager.LoadScene(HomeScene.Value);
+    }
+}
