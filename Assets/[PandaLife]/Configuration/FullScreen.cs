786ad4fbf26cec90eb126d28bcf1fbe1cdd99530
“Marcdaus” <mvmsueca@gmail.com>
2026-05-21 11:45:59 +0200

    menu configuracion

---
+++ Assets/[PandaLife]/Configuration/FullScreen.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+public class FullScreen : MonoBehaviour
+{
+    public Toggle toggle;
+    public TMP_Dropdown dropdownresolution;
+    Resolution[] resolutions;
+    private void Start()
+    {
+        if(Screen.fullScreen)
+        {
+            toggle.isOn = true;
+        }
+        else
+        {
+            toggle.isOn = false;
+        }
+        checkresolutions();
+    }
+    public void Fullscreen(bool fullscreen)
+    {
+        Screen.fullScreen = fullscreen;
+    }
+    public void checkresolutions()
+    {
+        resolutions = Screen.resolutions;
+        dropdownresolution.ClearOptions();
+        List<string> options = new List<string>();
+        int Currentresolution = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if(Screen.fullScreen && resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                Currentresolution = i;
+            }
+        }
+        dropdownresolution.AddOptions(options);
+        dropdownresolution.value = Currentresolution;
+        dropdownresolution.RefreshShownValue();
+    }
+    public void changeresolution(int resolutionindex)
+    {
+        Resolution resolution = resolutions[resolutionindex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+}
