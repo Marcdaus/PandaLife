11ca2de670594e67adffb06e16a094f11823a729
RAAAmajo <mrodalv@upv.edu.es>
2026-05-29 22:51:04 +0200

    sonido caminar pandas arreglado cosas
    EL SONIDO ES TEMPORAL, que no me ha gustao :(

--- Assets/[PandaLife]/Home/Interactives/Mini Pandas/SoundWalkingMinipanda.cs
+++ Assets/[PandaLife]/Home/Interactives/Mini Pandas/SoundWalkingMinipanda.cs
@@ -2,15 +2,14 @@ using UnityEngine;
 
 public class SoundWalkingMinipanda : MonoBehaviour
 {
+    //RECUERDA esto tu lo llamas desde el animation de walking con un evento dentro de la animation
+    //que esta escondidio
    public AudioSource audioSource;
 
-    // Este método lo sigues llamando desde el Animation Event
     public void ReproducirPaso()
     {
         if (audioSource != null)
         {
-            // Al darle Play, el AudioSource activará el contenedor 
-            // y este elegirá un sonido al azar automáticamente
             audioSource.Play(); 
         }
     }
