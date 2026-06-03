8fece568183842c603df6463359e915393b52bc9
Jose <josegandia2003@gmail.com>
2026-06-03 12:18:04 +0200

    Negacion randomizada

--- Assets/[PandaLife]/GlobalInteractives/Player/interactives/Scriptable_Objects/InteractableObject.cs
+++ Assets/[PandaLife]/GlobalInteractives/Player/interactives/Scriptable_Objects/InteractableObject.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Audio;
 
 [CreateAssetMenu(fileName = "InteractableObject", menuName = "Interactable Objects/InteractableObject")]
 public class InteractableObject : ScriptableObject
@@ -6,6 +7,6 @@ public class InteractableObject : ScriptableObject
     public string actionText = "Interactuar"; // Texto para la UI
     public string animationTrigger = "Interactuar"; // El trigger del Animator
     public AudioClip interactionSound; // Sonido único para este objeto
-    public AudioClip errorSound; // Sonido si el jugador se equivoca (Agitar la cabeza)
+    public AudioResource errorSound; // Sonido si el jugador se equivoca (Agitar la cabeza)
     public AudioClip dropSound; // Sonido si el jugador suelta el objeto
 }
