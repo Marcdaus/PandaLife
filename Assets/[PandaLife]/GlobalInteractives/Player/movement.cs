7367eedf98ce8a9078420cfa8d7ebbf46cc7b1e8
Jose <josegandia2003@gmail.com>
2026-06-08 16:27:39 +0200

    Fix movimiento player

--- Assets/[PandaLife]/GlobalInteractives/Player/movement.cs
+++ Assets/[PandaLife]/GlobalInteractives/Player/movement.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class movement : MonoBehaviour
@@ -20,7 +21,8 @@ public class movement : MonoBehaviour
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
-        Vector3 move = new Vector3(h, 0, v);
+        Vector3 move = new Vector3(h, 0, v).normalized;
+        Debug.Log($"Input: {move}");
 
         // Obtener la dirección de la cámara (solo eje Y)
         Vector3 camForward = Camera.main.transform.forward;
