b0bbeadcf66babeca731ca9a2d61f66dd48a247d
Lucia310506 <luciadiazmurcia9@gmail.com>
2026-06-10 18:35:15 +0200

    Primera versión MenuPausa

--- Assets/[PandaLife]/Pausemenu/PauseMenuController.cs
+++ Assets/[PandaLife]/Pausemenu/PauseMenuController.cs
@@ -1,7 +1,19 @@
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
 
 public class PauseMenuController : MonoBehaviour
 {
+    private bool isopen = false;
+    [SerializeField] private Player panda;
+    [SerializeField] private GameString HomeScene;
+    [SerializeField] private GameString MainmenuScene;
+    [SerializeField] private AudioMixerSnapshot normalAudio;
+    [SerializeField] private AudioMixerSnapshot pausedAudio;
+    [SerializeField] private GameObject pauseMask;
+    private bool paused = false;
+
+
     public void TimeStop()
     {
         if (GameManager.instance.stopTime == false)
@@ -24,5 +36,87 @@ public class PauseMenuController : MonoBehaviour
         }
     }
 
+    public void StopPanda()
+    {
+        if (!isopen)
+        {
+            panda.DisableMovement();
+        }
+        else
+        {
+            panda.EnableMovement();
+        }
+    }
+
+    public void StopMiniPandas()
+    {
+
+    }
+
+   
+    public bool Paused
+    {
+        get => paused;
+        set
+        {
+            if (paused == value) return;
+            paused = value;
+            if (paused)
+            {
+                pausedAudio.TransitionTo(1f);
+                pauseMask.SetActive(true);
+            }
+            else
+            {
+                normalAudio.TransitionTo(1f);
+                pauseMask.SetActive(false);
+            }
+        }
+    }
+    //====================================================
+    //                BBOTONES DEL MENÚ
+    //====================================================
+    public void OpenMenu()
+    {
+        isopen = true;
+
+        StopPanda();
+        StopMiniPandas();
+        StopBars();
+        TimeStop();
+        Paused = Paused;
+    }
+    public void Continue()
+    {
+        StopPanda();
+        StopMiniPandas();
+        StopBars();
+        TimeStop();
+        isopen= false;
+        Paused = !Paused;
+    } 
+    // Botón para el menú principal
+    public void GoToMainMenu()
+    {
+        isopen = false;
+        StopPanda();
+        StopMiniPandas();
+        StopBars();
+        TimeStop();
+        Paused = !Paused;
+        GameManager.instance.Resetplay();
+        SceneManager.LoadScene(MainmenuScene.Value);
+    }
+    public void Replay() 
+    { 
+        isopen = false;
+        StopPanda();
+        StopMiniPandas();
+        StopBars();
+        TimeStop();
+        Paused = !Paused;
+        GameManager.instance.Resetplay();
+        SceneManager.LoadScene(HomeScene.Value);
+    }
 
 }
