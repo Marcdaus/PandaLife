6baca39879467f791f55d431d3380e27c8905201
“Marcdaus” <mvmsueca@gmail.com>
2026-06-12 12:05:16 +0200

    Rematar sonidos del gameover y mas

--- Assets/[PandaLife]/GameOver/canvas/GameOverActions.cs
+++ Assets/[PandaLife]/GameOver/canvas/GameOverActions.cs
@@ -5,17 +5,18 @@ public class GameOverActions : MonoBehaviour
 {
     [SerializeField] private GameString HomeScene;
     [SerializeField] private GameString MainmenuScene;
-
+    [SerializeField] private AudioClip buttonSound;
 
     public void MainMenu()
     {
+        SoundManager.instance.PlaySfx(buttonSound);
         GameManager.instance.Resetplay();
         SceneManager.LoadScene(MainmenuScene.Value);
 
     }
     public void Replay()
     {
-
+        SoundManager.instance.PlaySfx(buttonSound);
         GameManager.instance.Resetplay();
         SceneManager.LoadScene(HomeScene.Value);
     }
